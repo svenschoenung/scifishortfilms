509799ef988a4fbf7648e840c22dce660f8a32d3
Sven Schoenung <sven.schoenung@gmail.com>
2017-03-03 21:08:26 +0100

    Merge inline script blocks

--- src/server/templates/Index.jsx
+++ src/server/templates/Index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
 export default function Index({app, config, head, state}) {
-  var bundleScript = `
+  var script = `
+    window.__STORE_STATE__ = ${JSON.stringify(state).replace(/</g, '\\u003c')};
     (function() {
       var script = document.createElement('script');
       script.setAttribute('src', '/${config.bundle}');
       document.getElementsByTagName('head')[0].appendChild(script);
     })();
   `;
-  var stateScript = `
-    window.__STORE_STATE__ = ${JSON.stringify(state).replace(/</g, '\\u003c')};
-  `;
-  
+ 
   return (
     <html>
     <head>
@@ -19,8 +17,7 @@ export default function Index({app, config, head, state}) {
     </head>
     <body>
       <div id="app" dangerouslySetInnerHTML={{__html:app}}/>
-      <script dangerouslySetInnerHTML={{__html:stateScript}}/>
-      <script dangerouslySetInnerHTML={{__html:bundleScript}}/>
+      <script dangerouslySetInnerHTML={{__html:script}}/>
     </body>
     </html>
   );
